8c3304b30d95e4009edb047ddc4307ec853cb1d4
Heriberto Monterrubio <heri185403@gmail.com>
2016-11-26 00:52:41 -0600

    configuira email de contacto

--- resources/views/admin/mail_contact_requested.blade.php
+++ resources/views/admin/mail_contact_requested.blade.php
@@ -3,5 +3,7 @@
     <div>Nombre: {{ $contact->name }}</div>
     <div>Email: {{ $contact->email }}</div>
     <div>Teléfono: {{ $contact->phone }}</div>
+    <div>WhatsApp: {{ $contact->whatsapp }}</div>
+    <div>País: {{ $contact->country }}</div>
     <div>Comentarios: {{ $contact->comments }}</div>
 </div>
